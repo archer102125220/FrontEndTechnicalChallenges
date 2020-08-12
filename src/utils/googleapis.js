6aecc101888d9e68243f9812feae58124ffa4932
陳柏杰 <archer10212522.2015@gmail.com>
2020-08-13 00:00:02 +0800

    googleapis hoc

--- src/utils/googleapis.js
+++ src/utils/googleapis.js
@@ -1,99 +1,82 @@
-import React, { Component } from 'react';
-import PropTypes from 'PropTypes';
-import GoogleMapReact from 'google-map-react';
+// import React, { Component } from 'react';
+// import PropTypes from 'PropTypes';
+// import GoogleMapReact from 'google-map-react';
 
 //https://www.npmjs.com/package/google-map-react
+
+/*document.getElementsByTagName('heade')[0].appendChild(() => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=' + key + '&libraries=places&callback=initMap';
+    return script;
+});*/
+import React, { Component } from 'react';
+import { compose, withProps, withHandlers, withState } from 'recompose';
+import { withScriptjs, withGoogleMap, Marker } from 'react-google-maps';
+const { GoogleMap: GoogleMapReact } = require('react-google-maps');
+
 const key = process.env.GOOGLE_MAP_API_KEY;
 
-export class GoogleMap extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            refs: {
-                map: undefined,
+const MyMapComponent = compose(
+    withProps({
+        googleMapURL: 'https://maps.googleapis.com/maps/api/js?key=' + key + '&v=3.exp&libraries=geometry,drawing,places',
+        loadingElement: <div style={{ height: '94vh' }} />,
+        containerElement: <div style={{ height: '94vh' }} />,
+        mapElement: <div style={{ height: '94vh' }} />,
+    }),
+    withScriptjs,
+    withGoogleMap,
+    withState('places', 'updatePlaces', ''),
+    withHandlers(() => {
+        const refs = {
+            map: undefined,
+        };
+
+        return {
+            onMapMounted: () => ref => {
+                refs.map = ref;
+            },
+            fetchPlaces: ({ updatePlaces }) => {
+                // eslint-disable-next-line no-unused-vars
+                let places;
+                const bounds = refs.map.getBounds();
+                // eslint-disable-next-line no-undef
+                const service = new google.maps.places.PlacesService(refs.map.context.__SECRET_MAP_DO_NOT_USE_OR_YOU_WILL_BE_FIRED);
+                const request = {
+                    bounds: bounds,
+                    type: ['restaurant']
+                };
+                service.nearbySearch(request, (results, status) => {
+                    // eslint-disable-next-line no-undef
+                    if (status == google.maps.places.PlacesServiceStatus.OK) {
+                        console.log(results);
+                        updatePlaces(results);
+                    }
+                });
             }
         };
-    }
+    }),
+)((props) => {
+    return (
+        <GoogleMapReact
+            onTilesLoaded={props.fetchPlaces}
+            ref={props.onMapMounted}
+            onBoundsChanged={props.fetchPlaces}
+            defaultZoom={8}
+            defaultCenter={{ lat: 51.508530, lng: -0.076132 }}
+        >
+            {props.places && props.places.map((place, i) =>
+                <Marker key={i} position={{ lat: place.geometry.location.lat(), lng: place.geometry.location.lng() }} />
+            )}
+        </GoogleMapReact>
+    );
+});
 
-    MapIcon = ({ element, style }) => <div style={style}>{element}</div>
-
+export class GoogleMap extends Component {
     render() {
-        const { MapIcon, props } = this;
-        const { center, zoom, place, height, width, style, mapIconStyle } = props;
-        const newStyle = {
-            height,
-            width,
-            '& div': {
-                padding: '0px 6px !important',
-            },
-            ...style
-        }, newMapIconStyle = {
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
-            height: '60px',
-            width: '60px',
-            borderRadius: '50%',
-            display: 'flex',
-            alignItems: 'center',
-            fontSize: '12px',
-            color: '#fff',
-            textAlign: 'center',
-            marginTop: '-30px',
-            marginLeft: '-30px',
-            ...mapIconStyle
-        };
         return (
-            <div style={newStyle}>
-                <GoogleMapReact
-                    onMapMounted={() => ref => {
-                        const { refs } = this.state;
-                        console.log({ refs });
-                        this.setState({ refs: { ...refs, map: ref } });
-                    }}
-                    bootstrapURLKeys={{ key }}
-                    defaultCenter={center}
-                    defaultZoom={zoom}
-                >
-                    {
-                        place.map((element, key) =>
-                            (<MapIcon
-                                key={key}
-                                lat={element.lat}
-                                lng={element.lng}
-                                text={element.element}
-                                style={newMapIconStyle}
-                            />)
-                        )
-                    }
-                </GoogleMapReact>
-            </div>
+            <MyMapComponent />
         );
     }
-
-    static defaultProps = {
-        center: {
-            lat: 59.95,
-            lng: 30.33
-        },
-        zoom: 13,
-        place: [{
-            lat: 59.955413,
-            lng: 30.337844
-        }],
-        height: '94vh',
-        width: '100%',
-        style: {},
-        mapIconStyle: {}
-    }
-
-    static propTypes = {
-        center: PropTypes.object,
-        zoom: PropTypes.number,
-        place: PropTypes.array,
-        height: PropTypes.string,
-        width: PropTypes.string,
-        style: PropTypes.object,
-        mapIconStyle: PropTypes.object
-    }
 }
-
 //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=24.985175,121.440307&radius=500&type=restaurant&keyword=餐廳&key=
